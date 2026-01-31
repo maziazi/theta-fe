ad2fd209303bb32d1a8bc46afb66aa1fd7d0049f
Muhamad Azis <mazis9651@gmail.com>
2026-01-31 15:23:25 +0700

    fix: manifest in mini app

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -15,26 +15,24 @@ return Response.json(
   },
   "miniapp": {
     "version": "1",
-    "name": "Example Mini App",
-    "homeUrl": "https://ex.co",
-    "iconUrl": "https://ex.co/i.png",
-    "splashImageUrl": "https://ex.co/l.png",
+    "name": "Nawasena",
+    "homeUrl": URL,
+    "iconUrl": `${URL}/logo/Logo-Nawasena.png`,
+    "splashImageUrl": `${URL}/logo/Logo-Nawasena.png`,
     "splashBackgroundColor": "#000000",
-    "webhookUrl": "https://ex.co/api/webhook",
+    "webhookUrl": `${URL}/api/webhook`,
     "subtitle": "Fast, fun, social",
     "description": "A fast, fun way to challenge friends in real time.",
     "screenshotUrls": [
-      "https://ex.co/s1.png",
-      "https://ex.co/s2.png",
-      "https://ex.co/s3.png"
+      `${URL}/logo/Logo-Nawasena.png`
     ],
     "primaryCategory": "social",
-    "tags": ["example", "miniapp", "baseapp"],
-    "heroImageUrl": "https://ex.co/og.png",
+    "tags": ["nawasena", "miniapp", "baseapp"],
+    "heroImageUrl": `${URL}/logo/Logo-Nawasena.png`,
     "tagline": "Play instantly",
-    "ogTitle": "Example Mini App",
+    "ogTitle": "Nawasena",
     "ogDescription": "Challenge friends in real time.",
-    "ogImageUrl": "https://ex.co/og.png",
+    "ogImageUrl": `${URL}/logo/Logo-Nawasena.png`,
     "noindex": true
   }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import { redirect } from 'next/navigation'
+'use client'
+
+import { useRouter } from 'next/navigation'
 import { sdk } from '@farcaster/miniapp-sdk';
 import { useEffect } from 'react';
 
 export default function RootPage() {
-  useEffect(() => {
-        sdk.actions.ready();
-    }, []);
+  const router = useRouter();
 
-  redirect('/home')
+  useEffect(() => {
+    sdk.actions.ready();
+    router.push('/home');
+  }, [router]);
+
+  return null;
 }
